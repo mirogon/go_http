723855ecb79a414cab0b47d26aac2043a33110c9
Marcel Roemer <marcelroemersoftware@gmail.com>
2023-10-28 18:30:28 +0200

    fixed tests

--- test/cookie_test.go
+++ test/cookie_test.go
@@ -8,31 +8,31 @@ import (
 )
 
 func TestCookie(t *testing.T) {
-	cookie := http_.CreateCookie("name", "value", true, false, nil)
+	cookie := http_.CreateCookie("name", "value", true, false, "", nil)
 	if cookie.GetValueString() != "name=value; HttpOnly" {
 		t.Error("!" + cookie.GetValueString() + "!")
 	}
 
-	cookie = http_.CreateCookie("name", "value", true, true, nil)
+	cookie = http_.CreateCookie("name", "value", true, true, "", nil)
 	if cookie.GetValueString() != "name=value; HttpOnly; Secure" {
 		t.Error("!" + cookie.GetValueString() + "!")
 	}
 
-	cookie = http_.CreateCookie("name", "value", false, false, nil)
+	cookie = http_.CreateCookie("name", "value", false, false, "", nil)
 	if cookie.GetValueString() != "name=value" {
 		t.Error("!" + cookie.GetValueString() + "!")
 	}
 
 	loc, _ := time.LoadLocation("GMT")
 	someTime := time.Date(2023, 1, 1, 0, 7, 9, 0, loc)
-	cookie = http_.CreateCookie("name", "value", false, false, &someTime)
+	cookie = http_.CreateCookie("name", "value", false, false, "", &someTime)
 	if cookie.GetValueString() != "name=value; Expires=Sun, 1 Jan 2023 00:07:09 GMT" {
 		t.Error("!" + cookie.GetValueString() + "!")
 	}
 
 	loc, _ = time.LoadLocation("GMT")
 	someTime = time.Date(2023, 1, 1, 0, 7, 9, 0, loc)
-	cookie = http_.CreateCookie("name", "value", true, true, &someTime)
+	cookie = http_.CreateCookie("name", "value", true, true, "", &someTime)
 	if cookie.GetValueString() != "name=value; Expires=Sun, 1 Jan 2023 00:07:09 GMT; HttpOnly; Secure" {
 		t.Error("!" + cookie.GetValueString() + "!")
 	}
